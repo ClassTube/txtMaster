2b3a641224553b7eeb030c26ec66158fda63bc4c
adityasirji <146421388+adityasirji@users.noreply.github.com>
2023-11-04 16:07:51 +0530

    Update details.py

--- details.py
+++ details.py
@@ -1,4 +1,4 @@
 api_id = 29257953
 api_hash = "31dae8eac39b38583898e3bc78b96406"
-bot_token = "6372655650:AAFhHd4xiZTCpUe7Ntxq6GzhCtpJparfpQk"
-sudo_groups = -4072787950
+bot_token = "6949360114:AAEo1aIGcBl9cq0_fhzDTlgODT72sRcDtPo"
+sudo_groups = -4038286914
