636a5c52941636f9c5bd7a9cfd8c0ede884a5a55
Yashdeepsharma2 <139551442+Yashdeepsharma2@users.noreply.github.com>
2023-11-05 16:18:11 +0530

    Update details.py

--- details.py
+++ details.py
@@ -1,4 +1,4 @@
 api_id = 27097807
 api_hash = "9fd790a9cb1f639c921d941621d2959d"
 bot_token = "6351714036:AAFFameNDrmVv9fRHuLJ7r1mL0llIzx7NTM"
-sudo_groups =-1002074844997
+sudo_groups = -1002074844997
