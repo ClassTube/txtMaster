8e8a5c4ee9ea894ac01fa64cedad2b4601e453ef
Yashdeepsharma2 <139551442+Yashdeepsharma2@users.noreply.github.com>
2023-11-05 17:08:42 +0530

    Update helper.py

--- helper.py
+++ helper.py
@@ -1,5 +1,6 @@
 import subprocess
 import datetime
+import json
 import asyncio
 import os
 import requests
@@ -10,6 +11,7 @@ import tgcrypto
 import aiofiles
 from pyrogram.types import Message
 from pyrogram import Client, filters
+from subprocess import getstatusoutput
 
 def duration(filename):
     result = subprocess.run(["ffprobe", "-v", "error", "-show_entries",
@@ -194,3 +196,20 @@ async def send_vid(bot: Client, m: Message,cc,filename,thumb,name,prog):
     os.remove(f"{filename}.jpg")
     await reply.delete (True)
     
+def get_video_attributes(file: str):
+    """Returns video duration, width, height"""
+
+    class FFprobeAttributesError(Exception):
+        """Exception if ffmpeg fails to generate attributes"""
+
+    cmd = (
+        "ffprobe -v error -show_entries format=duration "
+        + "-of default=noprint_wrappers=1:nokey=1 "
+        + "-select_streams v:0 -show_entries stream=width,height "
+        + f" -of default=nw=1:nk=1 '{file}'"
+    )
+    res, out = getstatusoutput(cmd)
+    if res != 0:
+        raise FFprobeAttributesError(out)
+    width, height, dur = out.split("\n")
+    return (int(float(dur)), int(width), int(height))
