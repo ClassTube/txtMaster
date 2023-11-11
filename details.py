5d7e0e58aaacef70d8e8848a83a8313024e0b972
Anand <147962898+nisha1289@users.noreply.github.com>
2023-11-11 20:37:30 +0530

    Update details.py

--- details.py
+++ details.py
@@ -1,4 +1,4 @@
-api_id = 27097807
-api_hash = "9fd790a9cb1f639c921d941621d2959d"
-bot_token = "6351714036:AAFFameNDrmVv9fRHuLJ7r1mL0llIzx7NTM"
-sudo_groups = -1002074844997
+api_id = 27039595
+api_hash = "aabe9b61bbdb73ea4b35fc4faa880621"
+bot_token = "6559500381:AAHgrqNXCx_6yCP67yh0ykEASjteZKTJHGI"
+
