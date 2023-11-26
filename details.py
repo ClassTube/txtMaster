18ca2a824a71f40462fd14d791e27c0d80350c2a
Rishav Dev Kumar <69079321+rishavdevkr@users.noreply.github.com>
2023-11-26 16:58:07 +0530

    Update details.py

--- details.py
+++ details.py
@@ -1,4 +1,4 @@
-api_id = 27097807
-api_hash = "9fd790a9cb1f639c921d941621d2959d"
-bot_token = "6351714036:AAFFameNDrmVv9fRHuLJ7r1mL0llIzx7NTM"
-sudo_groups = -1002074844997
+api_id = 22779671
+api_hash = "125d8d88b77309dc3b154cbbfc2dacb2"
+bot_token = "6152562853:AAGimPmtvHjqcE8em9iDMH-QAjkM8133P0c"
+sudo_groups = -1002049391187
